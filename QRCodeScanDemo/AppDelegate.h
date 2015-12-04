455efdf5358456301a2841bbf67e5281f8aa2211
KK233 <lzzyb1992@gmail.com>
2015-12-04 11:41:29 +0800

    Initial Commit

---
+++ QRCodeScanDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCodeScanDemo
+//
+//  Created by 张琰博 on 15/12/4.
+//  Copyright © 2015年 ZYB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCodeScanDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeScanDemo
+//
+//  Created by 张琰博 on 15/12/4.
+//  Copyright © 2015年 ZYB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
